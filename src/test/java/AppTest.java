836d8e2ecb40dfbfce1b01e651b937d359634f95
Nguyen Duc Anh <nguyenducanhhihi7@gmail.com>
2026-05-10 22:16:00 +0700

    czcscasc

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,9 @@
-//import org.junit.jupiter.api.Test;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class AppTest {
-//    @Test
-//    public void testAlwaysFails() {
-//        assertTrue(false, "Tính năng tính toán bị sai logic!");
-//    }
-//}
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    public void testAlwaysFails() {
+        assertTrue(false, "Tính năng tính toán bị sai logic!");
+    }
+}
