9c11208eda8b7fd596fcbc237cee78d795c596f0
Nguyen Duc Anh <nguyenducanhhihi7@gmail.com>
2026-05-11 08:03:55 +0700

    czcscasc

--- src/test/java/FileManagerTest.java
+++ src/test/java/FileManagerTest.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -5,8 +6,14 @@ public class FileManagerTest {
     @Test
     public void testCreatePath() {
         FileManager fm = new FileManager();
-        // Test này mong đợi định dạng Windows
+
+        // Output của hàm (luôn bị hardcode là dấu \)
         String result = fm.createPath("data", "report.txt");
-        assertEquals("data\\report.txt", result);
+
+        // Giá trị mong đợi ĐÚNG PHẢI LÀ dấu của hệ điều hành đang chạy
+        String expected = "data" + File.separator + "report.txt";
+
+        // Tiến hành so sánh
+        assertEquals(expected, result, "Đường dẫn bị sai định dạng OS!");
     }
 }
