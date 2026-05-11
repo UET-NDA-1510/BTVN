8085a215b4b014d5f08dff28effe77cf32eb0b44
Nguyen Duc Anh <nguyenducanhhihi7@gmail.com>
2026-05-11 15:34:34 +0700

    czcscasc

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        System.out.println("Xin chao the gioi");
+        System.out.println("Xin chao the ");
         System.out.println("Hello word");
     }
 }
